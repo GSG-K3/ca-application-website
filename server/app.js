0fb3770bc9c73a30b175f110b6cfeb5bb6c8848f
Nujood <shawar.nujood@gmail.com>
2020-04-22 17:40:26 +0300

    Setup Server #43

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {join}= require('path')
+const cookieParser = require("cookie-parser");
+const router = require("./controllers");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(router);
+
+// app.use(express.static(join(__dirname,'..','public')))
+
+module.exports = app;
