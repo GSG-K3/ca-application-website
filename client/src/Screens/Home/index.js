6b10aa221404d5460117ee79b3ca480d6bfe4b22
karmelyoei <karmel.salah@gmail.com>
2020-04-28 22:36:26 +0300

    updating the file to contain only the connetion between the frontend and the backend with a home page #65

--- client/src/Screens/Home/index.js
+++ client/src/Screens/Home/index.js
@@ -1,12 +1,10 @@
 import React, { Fragment, Component } from 'react';
-import NavBar from '../../Components/NavBar';
 
 class Home extends Component {
 	state = {};
 	render() {
 		return (
 			<Fragment>
-				<NavBar />
 				<h1>Hello this is a home screen</h1>
 			</Fragment>
 		);
