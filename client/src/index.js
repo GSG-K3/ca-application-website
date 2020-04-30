70e27b91c64609a06459ecb96f3fb5a8d4fe749c
Duha96 <141046@ppu.edu.ps>
2020-04-30 16:19:28 +0300

    resolved conflict #42

--- client/src/index.js
+++ client/src/index.js
@@ -7,23 +7,12 @@ import { MuiThemeProvider } from '@material-ui/core/';
 import theme from './theme';
 
 ReactDOM.render(
-<<<<<<< HEAD
 	<MuiThemeProvider theme={theme}>
 		<React.StrictMode>
 			<App />
 		</React.StrictMode>
 	</MuiThemeProvider>,
 	document.getElementById('root'),
-=======
-
-  <MuiThemeProvider theme={theme}>
-  <React.StrictMode >
-    <App />
-  </React.StrictMode>
-  </MuiThemeProvider>,
-  document.getElementById('root')
-
->>>>>>> 8d8f948584dc0764ebc35e222410ef28787afe0e
 );
 
 // If you want your app to work offline and load faster, you can change
