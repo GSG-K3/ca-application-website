b18dee125ebef32c02aaa8182f033a6e3e53cabe
Rand Inaim <rinaim@asaltech.com>
2020-05-11 17:18:10 +0300

    connecting build file for deployment

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { join } = require('path');
 const cookieParser = require('cookie-parser');
+const favicon = require('serve-favicon');
 const { router, secondRouter, thirdRouter } = require('./controllers/');
 const app = express();
 
@@ -9,7 +10,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(router, secondRouter, thirdRouter);
 
+app.use(favicon(join(__dirname, '..', 'client', 'build', 'favicon.ico')));
 app.use(express.static(join(__dirname, '..', 'client', 'build')));
+
+app.get('/*', function (req, res) {
+	res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+});
 
 app.set('port', process.env.PORT || 4000);
 
