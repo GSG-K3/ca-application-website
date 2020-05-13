2ed5969a1904e17efac9c2e9f4b99c55b45e748c
Nujood <shawar.nujood@gmail.com>
2020-05-13 17:28:38 +0300

    completing slideShow

--- client/src/Screens/Home/index.jsx
+++ client/src/Screens/Home/index.jsx
@@ -8,32 +8,7 @@ class Home extends Component {
 		return (
 			<Fragment>
 				 <SuccessStories />
-				{/* <Carousel>
-					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src="https://source.unsplash.com/random"
-							alt="First slide"
-						/>
-						<Carousel.Caption>
-							<h3>First slide label</h3>
-							<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-						</Carousel.Caption>
-					</Carousel.Item>
-					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src="https://source.unsplash.com/random"
-							alt="Third slide"
-						/>
-
-						<Carousel.Caption>
-							<h3>Second slide label</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-						</Carousel.Caption>
-					</Carousel.Item>
-					
-				</Carousel> */}
+			
 			</Fragment>
 		);
 	}
