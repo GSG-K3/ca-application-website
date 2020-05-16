e98e3873a227f6cabbafbbc897a6e55d3c8ee8cd
Rand Inaim <rinaim@asaltech.com>
2020-05-17 02:38:49 +0300

    home screen changes

--- client/src/screens/Home/index.js
+++ client/src/screens/Home/index.js
@@ -1,71 +1,43 @@
 import React, { Fragment, Component } from 'react';
 import axios from 'axios';
-import Requierments from '../../Components/Requirments';
+import Requierments from '../../Components/Requierments';
+import About from '../../Components/About';
 import NavBar from '../../Components/NavBar';
 import SupportTeam from '../../Components/SupportTeam';
 import Footer from '../../Components/Footer';
-import SuccessStories from '../../Components/SuccessStories';
+
 import { withStyles } from '@material-ui/core';
 import styles from './style';
-import { css } from '@emotion/core';
-import About from '../../Components/About';
-import * as Spinners from 'react-spinners';
-import { BounceLoader } from 'react-spinners';
-
-const override = css`
-	display: block;
-	margin: 0 auto;
-	border-color: red;
-`;
 class Home extends Component {
 	state = {
 		data: [],
-		isLoading: true,
 	};
 	componentDidMount() {
 		axios
 			.get('/home/support-team')
 			.then((result) => {
 				const { data } = result;
-				this.setState({ isLoading: false, data });
+				this.setState({ data });
 			})
-
-			.catch((err) => {
-				this.setState({ isLoading: false });
-				console.log(err);
-			});
+			.catch((err) => console.log(err));
 	}
 
 	render() {
 		const { classes } = this.props;
-		const { isLoading } = this.state;
+		console.log('home props', this.props);
 		return (
-			<>
-				{isLoading ? (
-					<div style={{ position: 'relative' }}>
-						<div className={classes.loader}>
-							<BounceLoader
-								// css={override}
-								className={classes.loader}
-								size={170}
-								color={' #4CB7A7'}
-								isLoading={isLoading}
-							/>
-						</div>
-					</div>
-				) : (
-					<Fragment id="home" className={classes.body}>
-						<NavBar matchPath={this.props.match} />
-						<About />
-						<Requierments />
-						<SuccessStories />
-						<SupportTeam data={this.state.data} />
-						<Footer />
-					</Fragment>
-				)}
-			</>
+			<Fragment>
+				<h2>this is home page</h2>
+				<div id="home" className={classes.body}>
+					<NavBar matchPath={this.props.match} />
+					<About />
+					<Requierments />
+					<SupportTeam data={this.state.data} />
+					<Footer />
+				</div>
+			</Fragment>
 		);
 	}
 }
 
-export default withStyles(styles)(Home);
+export default Home
