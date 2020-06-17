f4d5846374f0b2619223eecdadcdda6677d5df1c
Nujood <shawar.nujood@gmail.com>
2020-06-17 22:54:02 +0300

    fixing the token & middleware functionality

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,43 +1,38 @@
-const getUserByEmail = require('../database/queries/db_user');
-const { compareHashed, loginValidation } = require('../helper');
-const { sign } = require('jsonwebtoken');
+const getUserByEmail = require("../database/queries/db_user");
+const { compareHashed, loginValidation } = require("../helper");
+const { sign } = require("jsonwebtoken");
 const { SECRET } = process.env;
 
 const Login = (request, response) => {
-	let { user } = request.body;
-	const { email, password } = user;
-	const { error } = loginValidation(user);
-	if (typeof error !== 'undefined') {
-		return response.send(error.toString().replace('validationError:', ''));
-	}
-	getUserByEmail(email, (error, result) => {
-		console.log('QUERY RESULT!', result);
-		if (result.rowCount === 0) return response.send('no user exist in the db');
-		const data = result.rows[0];
-		if (data !== null) {
-			compareHashed(password, data.password)
-				.then((result) => {
-					console.log('PASSWORD RESULT!', result);
-					if (result) {
-						const token = sign({ email }, SECRET);
-						let info = {
-							user_id: data.user_id,
-							name: data.name,
-							email: data.email,
-						};
-						response
-							.status(200)
-							.cookie('token', token, { maxAge: 900000, httpOnly: true })
-							.json({ info, token });
-					} else {
-						response.status(400).json({ message: 'incorrect password' });
-					}
-				})
-				.catch((err) => {
-					response.status(500);
-				});
-		}
-	});
+  let { user } = request.body;
+  let { email, password } = user;
+  const { error } = loginValidation(user);
+  if (typeof error !== "undefined") {
+    return response.send(error.toString().replace("validationError:", ""));
+  }
+
+  getUserByEmail(user)
+    .then((result) => {
+      if (result.rowCount === 0)
+        return response.send("no user exist in the db");
+      let data = result.rows[0];
+      if (data !== null) {
+        compareHashed(password, data.password).then((result) => {
+          if (result) {
+            const { user_id } = data;
+            const token = sign({ email, user_id }, SECRET, { expiresIn: "1d" });
+            return response
+              .status(200)
+              .cookie("token", token)
+              .json({ massege: "successfully logged", token });
+          }
+          return response.json({ massege: "bad request" });
+        });
+      }
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
 };
 
 module.exports = Login;
---
+++ server/middleware/auth.js
@@ -0,0 +1,17 @@
+const { verify } = require("jsonwebtoken");
+const { SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  if (req.cookies && req.cookies.token) {
+    let { token } = req.cookies;
+    verify(token, SECRET, (error, result) => {
+      if (result) {
+        req.user_id = result.user_id;
+        return res.status(200).json({ message: "Authorized" });
+      }
+      if (error) return res.status(401).json({ message: "unAuthorized" });
+      return next();
+    });
+  }
+  return res.status(401).json({ message: "please login to continue.." });
+};
