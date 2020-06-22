1b24cf9c34dd85c234c6c2fd9b156cf0ca82b5c3
karmelyoei <karmel.salah@gmail.com>
2020-06-22 17:47:36 +0300

    removing the err since its the same name inside the object

--- server/controllers/error.js
+++ server/controllers/error.js
@@ -1,9 +1,9 @@
 const notFound = (err, req, res, next) => {
-	res.status(404).json({ message: '404 PAGE NOT FOUND', err: err });
+	res.status(404).json({ message: '404 PAGE NOT FOUND', err });
 };
 
 const serverErr = (err, req, res, next) => {
-	res.status(500).json({ message: '500 SERVER ERROR', err: err });
+	res.status(500).json({ message: '500 SERVER ERROR', err });
 };
 
 module.exports = { notFound, serverErr };
