7493b9ee202d6d0a1c6dbbba3898f9ef489a034f
karmelyoei <karmel.salah@gmail.com>
2020-06-24 23:52:52 +0300

    editing the projects quiery

--- server/database/queries/projects.js
+++ server/database/queries/projects.js
@@ -3,7 +3,7 @@ const db_connection = require('../connection');
 let insertUserProjects = (user) => {
 	const sql = {
 		text:
-			'Update account SET user_id=$1, project1_link =$2, project2_link = $3 Where user_id=$1',
+			'Update account SET  project1_link =$2, project2_link = $3 Where user_id=$1',
 		values: [user.userId, user.firstProject, user.secondProject],
 	};
 	return db_connection.query(sql.text, sql.values);
